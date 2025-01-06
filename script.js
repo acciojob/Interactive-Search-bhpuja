7d9c9f1c7134f12a35ab045d51b7d6e7c3bc12f8
bhpuja <vpuja0421@gmail.com>
2025-01-06 13:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const searchDiv = document.querySelector('.search');
+  const input = document.querySelector('.input');
+  const button = document.querySelector('.btn');
+
+  button.addEventListener('click', () => {
+    searchDiv.classList.add('active');
+    input.focus();
+  });
+});
